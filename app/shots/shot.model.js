1a569818a70624827bd2f51618e7d9d11c16dec8
Jean Franco Pizarro Gonçalves <jegoncalves@fh.com.br>
2017-04-12 12:07:26 -0300

    developer descriptions in details page

--- app/shots/shot.model.js
+++ app/shots/shot.model.js
@@ -1,14 +1,15 @@
 "use strict";
 class Shot {
-    constructor(title, description, images, id, likes_count, likes_url, user, view_count) {
+    constructor(id, title, description, images, user, tags, likes_count, view_count, buckets_count) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.images = images;
-        this.id = id;
-        this.likes_count = likes_count;
-        this.likes_url = likes_url;
         this.user = user;
+        this.tags = tags;
+        this.likes_count = likes_count;
         this.view_count = view_count;
+        this.buckets_count = buckets_count;
     }
 }
 exports.Shot = Shot;
--- app/shots/shot.model.ts
+++ app/shots/shot.model.ts
@@ -1,13 +1,17 @@
 export class Shot {
 
     constructor (
+        public id: number,
+        
         public title: string,
         public description: string,
+
         public images: Object,
-        public id: number,
-        public likes_count: number,
-        public likes_url: string,
         public user: Object,
-        public view_count: number
+        public tags: Object,
+
+        public likes_count: number,
+        public view_count: number,
+        public buckets_count: number
     ) {}
 }
